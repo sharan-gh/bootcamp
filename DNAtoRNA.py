037b281c7b663a749c59d1e5a7c085809ca56a5f
sprakash@caltech.edu <Sharan@Sharan-Prakashs-MacBook.local>
2016-09-12 19:28:41 -0700

    added to docstring.

--- DNAtoRNA.py
+++ DNAtoRNA.py
@@ -1,5 +1,5 @@
 def rna(seq):
-    """Convert DNA sequence to RNA."""
+    """Convert DNA sequence to RNA. The returned sequences have the same capitalization as the input sequence."""
     #convert to uppercase
     seq = seq.upper()
     return seq.replace('T','U')
