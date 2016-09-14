8025f15d9ce11ff84e1c64b6796526e0c76b6c62
sprakash@caltech.edu <Sharan@Sharan-Prakashs-MacBook.local>
2016-09-13 22:00:52 -0700

    some annotation on this exercise, and actually this is exercise 3 not 5

--- l24e5.py
+++ l24e5.py
@@ -13,9 +13,12 @@ sns.set(rc=rc)
 xa_high = np.loadtxt('data/xa_high_food.csv', comments = '#')
 xa_low = np.loadtxt('data/xa_low_food.csv', comments = '#')
 
+#Here, we are calling the ecdf module for each dataset. Remember that the module gives you a tuple with two elements, and the way we
+#are assigning variables here is for the first element followed by the second element.
 x_high, y_high = bootcamp_utils.ecdf(xa_high)
 x_low, y_low =bootcamp_utils.ecdf(xa_low)
 
+#Next we plot both the high and low food data as ecdfs on the same plot (for comparison). 
 plt.plot(x_high, y_high, marker='.', linestyle='none', markersize=20)
 plt.plot(x_low, y_low, marker='.', linestyle='none', markersize=20, alpha=0.5)
 plt.xlabel('Cross-sectional area (um)')
