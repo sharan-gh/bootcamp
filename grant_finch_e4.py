4357eb72bd4231dddfb8298ed7cf5e2e4140aec2
sprakash@caltech.edu <Sharan@Sharan-Prakashs-MacBook.local>
2016-09-14 22:01:41 -0700

    last update, neater...only one function for each column rename

--- grant_finch_e4.py
+++ grant_finch_e4.py
@@ -7,19 +7,8 @@ grant_1991 = pd.read_csv('data/grant_1991.csv', comment='#')
 
 
 grant_1973 = grant_1973.rename(columns={"yearband":"year"})
-grant_1973.rename(index='[]' , columns={"yearband":"year"})
 
-grant_1973 = grant_1973.rename(columns={"beak length":"beak length (mm)"})
-grant_1973 = grant_1973.rename(columns={"beak depth":"beak depth (mm)"})
 
-grant_1975 = grant_1975.rename(columns={"Beak length, mm":"beak length (mm)"})
-grant_1975 = grant_1975.rename(columns={"Beak depth, mm":"beak depth (mm)"})
-
-grant_1987 = grant_1987.rename(columns={"Beak length, mm":"beak length (mm)"})
-grant_1987 = grant_1987.rename(columns={"Beak depth, mm":"beak depth (mm)"})
-
-grant_1991 = grant_1991.rename(columns={"blength":"beak length (mm)"})
-grant_1991 = grant_1991.rename(columns={"Beak depth, mm":"beak depth (mm)"})
 
 
 grant_1973.ix[:,'year']=1973
@@ -30,3 +19,10 @@ grant_1975['year']=np.ones(len(grant_1975), dtype=int)*1975
 grant_1987['year']=np.ones(len(grant_1987), dtype=int)*1987
 grant_1991['year']=np.ones(len(grant_1991), dtype=int)*1991
     #what we want is a column of ones that is len(df). We then multiply each element (each one) by the number we want. Cool!
+
+grant_1973 = grant_1973.rename(columns={"beak length":"beak length (mm)"})
+grant_1973 = grant_1973.rename(columns={"beak depth":"beak depth (mm)"})
+grant_1975.columns = ['band', 'species', 'beak length (mm)', 'beak depth (mm)', 'year']
+grant_1987.columns = ['band', 'species', 'beak length (mm)', 'beak depth (mm)', 'year']
+grant_1991.columns = ['band', 'species', 'beak length (mm)', 'beak depth (mm)', 'year']
+grant_2012.columns = ['band', 'species', 'beak length (mm)', 'beak depth (mm)', 'year']
