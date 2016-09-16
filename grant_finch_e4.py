beb70102df937dbd4b15c8eabf864728b9f31426
sprakash@caltech.edu <Sharan@Sharan-Prakashs-MacBook.local>
2016-09-15 18:34:20 -0700

    concatenated dataframes

--- grant_finch_e4.py
+++ grant_finch_e4.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import numpy as np
+#import datasets
 grant_1973 = pd.read_csv('data/grant_1973.csv', comment='#')
 grant_1975 = pd.read_csv('data/grant_1975.csv', comment='#')
 grant_1987 = pd.read_csv('data/grant_1987.csv', comment='#')
 grant_1991 = pd.read_csv('data/grant_1991.csv', comment='#')
 
-
+#fix year column name
 grant_1973 = grant_1973.rename(columns={"yearband":"year"})
-
-
-
 
 grant_1973.ix[:,'year']=1973
 
@@ -22,7 +20,8 @@ grant_1991['year']=np.ones(len(grant_1991), dtype=int)*1991
 
 grant_1973 = grant_1973.rename(columns={"beak length":"beak length (mm)"})
 grant_1973 = grant_1973.rename(columns={"beak depth":"beak depth (mm)"})
+
+#Columns renamed
 grant_1975.columns = ['band', 'species', 'beak length (mm)', 'beak depth (mm)', 'year']
 grant_1987.columns = ['band', 'species', 'beak length (mm)', 'beak depth (mm)', 'year']
 grant_1991.columns = ['band', 'species', 'beak length (mm)', 'beak depth (mm)', 'year']
-grant_2012.columns = ['band', 'species', 'beak length (mm)', 'beak depth (mm)', 'year']
