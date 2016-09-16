ed292bf5616b1ca77e7b9532ac62f09fb7321ceb
sprakash@caltech.edu <Sharan@Sharan-Prakashs-MacBook.local>
2016-09-15 17:19:21 -0700

    fixed indentation errors

--- bootstrapping_function.py
+++ bootstrapping_function.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pandas as pd
 import seaborn as sbn
-import matplotlib.plotpy as plt
+
 
 def bs_reps(data, func, n_reps=100000):
-    '''Computes the bootstrapped confidence interval for test statistics according to numpy features and syntax. 100,000 replicates as default.'''
+    '''Computes the bootstrapped confidence interval for test statistics. 100,000 replicates as default.'''
     #Initialize the array of replicates. We will compute the confidence interval based on the distribution of means in this array.
     bs_func = np.empty(n_reps)
     #Here we are generating the bootstrap
@@ -13,6 +13,5 @@ def bs_reps(data, func, n_reps=100000):
         #Now you need to add this to the array...
         bs_func[i] = func(bs_sample)
         #and compute the confidence interval
-        conf_int_func = np.percentile(bs_func, [2.5, 97.5])
-        return
-        print(conf_int_func)
+    conf_int_func = np.percentile(bs_func, [2.5, 97.5])
+    return conf_int_func
